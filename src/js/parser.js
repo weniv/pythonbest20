b6fe02d2ce0e29e253502647b48d5719c4ca3350
paullabkorea <paullabkorea@gmail.com>
2023-01-01 17:49:47 +0900

    fetch 경로 수정

--- src/js/parser.js
+++ src/js/parser.js
@@ -267,7 +267,7 @@
     console.log(window.location)
     console.log(window.location.pathname)
     console.log(window.location.pathname.split('/'))
-    if (window.location.pathname.split('/')[0] === '') {
+    if (window.location.pathname.split('/')[1] === '' || window.location.pathname.split('/')[1] === 'index.html') {
       // 로컬 테스트 및 실제 배포
       const res = await fetch(
         `${window.location.origin}/src/pages/${PAGE_NAME}/article.md`
@@ -278,7 +278,7 @@
     } else {
       // github page url로 배포시
       const res = await fetch(
-        `${window.location.origin}/${window.location.pathname.split('/')[0]}/src/pages/${PAGE_NAME}/article.md`
+        `${window.location.origin}/${window.location.pathname.split('/')[1]}/src/pages/${PAGE_NAME}/article.md`
       );
       const markdown = await res.text();
       // console.log(markdown);
