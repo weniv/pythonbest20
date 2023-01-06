881e2eb392e8521a7015f03e349b004c98c2d43f
paullabkorea <paullabkorea@gmail.com>
2023-01-06 09:49:09 +0900

    url에 따른 문제 보기 구현

--- src/env.js
+++ src/env.js
@@ -1,2 +1,13 @@
 let PAGE_NAME = 1;
 const BASE_URL = '';
+
+
+if (typeof (history.pushState) != "undefined") {
+    //console.dir(window.location)
+    if (window.location.search != "") {
+        PAGE_NAME = parseInt(window.location.search.split('=')[1])
+    }
+} else {
+    //브라우저가 지원하지 않는 경우 처리
+    PAGE_NAME = 1;
+}
