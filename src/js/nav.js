fbb22127de22f0a775e781c68f82d9b4ef70db6e
강혜진 <dreamfulbud@naver.com>
2023-01-13 10:08:58 +0900

    메뉴 기능 개선(resize, mobile 닫힘)

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,13 +1,55 @@
 const $container = document.querySelector(".container");
 const $btnFold = document.querySelector(".btn-fold");
+const $nav = document.querySelector("nav");
+let isMobile = null;
+
+const handleClose = () => {
+  $container.classList.remove("menu-on");
+  $btnFold.innerText = "메뉴 펼치기";
+}
+const handleOpen = () => {
+  $container.classList.add("menu-on");
+  $btnFold.innerText = "메뉴 접기";
+}
 
 const handleToggleMenu = () => {
   if ($container.classList.contains("menu-on")) {
-    $container.classList.remove("menu-on");
-    $btnFold.innerText = "메뉴 펼침";
+    handleClose();
   } else {
-    $container.classList.add("menu-on");
-    $btnFold.innerText = "메뉴 접힘";
+
+    handleOpen();
   }
 };
+
+
+
+const checkMobile = () => {
+  const winWidth = window.innerWidth;
+
+  if(winWidth > 1024){
+    isMobile = false;
+  }else{
+    handleClose();
+    isMobile = true;
+  }
+}
+
+const handleResizeWidth =() =>{
+  let timer = null;
+  clearTimeout(timer);
+  timer = setTimeout(function(){
+    checkMobile();
+  },300)
+}
+
+const handleCloseMobileMenu =(e) =>{
+  const check = Boolean(e.target.closest('nav')) || Boolean(e.target.closest('header'));
+  if(isMobile && $container.classList.contains("menu-on") && !check){
+    handleClose();
+  }
+}
+
+checkMobile();
 $btnFold.addEventListener("click", handleToggleMenu);
+$container.addEventListener("click", handleCloseMobileMenu);
+window.addEventListener("resize", handleResizeWidth);
