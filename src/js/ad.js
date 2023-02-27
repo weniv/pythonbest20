74f99ec141a27f90bb005986d579f0e8fdd44a4b
John Doe <leehy0782@naver.com>
2023-02-27 13:30:29 +0900

    Edit: 캐러셀에 좌우 이동 버튼 및 기능  추가

--- src/js/ad.js
+++ src/js/ad.js
@@ -1,6 +1,8 @@
 let cnt = 1;
-let time = 3000;
+const beforeBtn = document.querySelector("#ad-before");
+const nextBtn = document.querySelector("#ad-next");
 
+// 자동 화면 전환
 setInterval(() => {
   document.getElementById("radio" + cnt).checked = true;
   cnt++;
@@ -9,3 +11,30 @@ setInterval(() => {
     cnt = 1;
   }
 }, 3000);
+
+// next 버튼 클릭 시
+beforeBtn.addEventListener("click", () => {
+  if(cnt <= 1) {
+    cnt = 6;
+    document.getElementById("radio" + cnt).checked = true;
+    console.log(cnt)
+  } else {
+    cnt--;
+    document.getElementById("radio" + cnt).checked = true;
+    console.log(cnt);
+
+  }
+})
+
+// before 버튼 클릭 시
+nextBtn.addEventListener("click", () => {
+  if (cnt >= 6) {
+    cnt = 1;
+    document.getElementById("radio" + cnt).checked = true;
+    console.log(cnt);
+  } else {
+    cnt++;
+    document.getElementById("radio" + cnt).checked = true;
+    console.log(cnt);
+  }
+});
