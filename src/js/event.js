f49bb9e71bf7eceb9e0dd572e323c4c78365faf5
paullabkorea <paullabkorea@gmail.com>
2023-03-14 16:35:16 +0900

    다운로드 버튼 추가 및 기능 구현

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,8 @@
 const $btnQue = document.querySelectorAll('.btn-que');
 const $btnRun = document.querySelector("#btn-run");
 const $resultInfo = document.querySelector("#result_info");
+const $btnDownload = document.querySelector(".btn-download");
+
 
 // 문제 로딩
 $btnQue.forEach(element => {
@@ -10,7 +12,7 @@ $btnQue.forEach(element => {
         document.getElementById('q' + PAGE_NAME).classList.add('active')
         history.pushState(null, PAGE_NAME, `?page=${PAGE_NAME}`);
         // 문제 이동 시 에러 메시지 초기화
-        document.getElementById('result_desc').textContent='';
+        document.getElementById('result_desc').textContent = '';
         render();
         loadCode();
     });
@@ -37,4 +39,35 @@ $btnRun.addEventListener("click", (e) => {
     window.localStorage.setItem(PAGE_NAME, text);
     codeEditor.textContent = text
     $resultInfo.classList.add("result-info-none");
+});
+
+const downloadFile = async ({ data, fileName, fileType }) => {
+    const blob = new Blob([data], { type: fileType });
+    const link = document.createElement('a');
+
+    link.download = fileName;
+    link.href = await URL.createObjectURL(blob);
+
+    const clickEvt = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+    });
+    link.dispatchEvent(clickEvt);
+    link.remove();
+};
+
+$btnDownload.addEventListener("click", (e) => {
+    let localStorageValue = window.localStorage.getItem(PAGE_NAME);
+    if (!!localStorageValue) {
+        const name = `solution_${PAGE_NAME}`;
+        // localStorageValue.replace(/\n/g, ``);
+        downloadFile({
+            data: JSON.stringify(localStorageValue),
+            fileName: `${name}.md`,
+            fileType: 'text/json',
+        });
+    } else {
+        window.alert('다운로드 할 데이터가 없습니다.')
+    }
 });
