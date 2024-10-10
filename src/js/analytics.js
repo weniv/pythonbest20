ebda6fc0c727e51ad70ca36b891e353b9f7400e8
Iam-Ujin <dklvmf0106@naver.com>
2024-10-10 09:53:33 +0900

    🐛 Fix: 위니브 애널리틱스 코드 수정

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,105 +1,96 @@
-const BASE_URL = 'https://www.analytics.weniv.co.kr';
+// const BASE_URL = "https://www.analytics.weniv.co.kr";
 
 //------------------------------------------------------------
 // @post /collect/pageview
-function collectPageView(session_id) {
-  const header = {
-    'Content-Type': 'application/json',
-  };
-  const payload = {
-    url: window.location.href,
-  };
+function collectPageView() {
+    const this_page_url = window.location.href;
+    let session_id = sessionStorage.getItem("session_id");
 
-  if (session_id) {
-    header['Session-Id'] = session_id;
-    payload.session_id = session_id;
-  }
-
-  fetch(`${BASE_URL}/collect/pageview`, {
-    method: 'POST',
-    headers: header,
-    body: JSON.stringify(payload),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (!session_id) {
-        localStorage.setItem('session_id', data.session_id);
-      }
-    })
-    .catch((error) => console.error('Error:', error));
+    if (!session_id) {
+        fetch("https://www.analytics.weniv.co.kr/collect/pageview", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url: this_page_url }),
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                sessionStorage.setItem("session_id", data.session_id);
+            })
+            .catch((error) => console.error("Error:", error));
+    }
 }
-window.addEventListener('load', (e) => {
-  const session_id = localStorage.getItem('session_id');
-  const lastPage = localStorage.getItem('lastPage');
 
-  if (lastPage !== window.location.pathname) {
-    collectPageView(session_id);
-  }
+window.addEventListener("load", (e) => {
+    const lastPage = localStorage.getItem("lastPage");
 
-  localStorage.setItem('lastPage', window.location.pathname);
+    if (lastPage !== window.location.pathname) {
+        collectPageView();
+    }
+
+    localStorage.setItem("lastPage", window.location.pathname);
 });
 
 //------------------------------------------------------------
 // @post /collect/anchor-click
-async function collectAnchorClick(event, type) {
-  event.preventDefault(); // 기본 동작 막기
+async function collectAnchorClick(event, text) {
+    event.preventDefault(); // 기본 동작 막기
 
-  const ANCHOR = event.currentTarget;
+    const ANCHOR = event.currentTarget;
 
-  const session_id = localStorage.getItem('session_id');
+    const session_id = sessionStorage.getItem("session_id");
 
-  const source_url = window.location.href;
-  const target_url = ANCHOR.href;
-  const target_tar = ANCHOR.target || '_self';
+    const source_url = window.location.href;
+    const target_url = ANCHOR.href;
+    const target_tar = ANCHOR.target || "_self";
 
-  try {
-    const response = await fetch(`${BASE_URL}/collect/anchor-click`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Session-Id': session_id,
-      },
-      body: JSON.stringify({ source_url, target_url, type }),
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    try {
+        const response = await fetch(`https://www.analytics.weniv.co.kr/collect/anchor-click`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Session-Id": session_id,
+            },
+            body: JSON.stringify({ source_url, target_url, type: text }),
+        });
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+    } finally {
+        window.open(target_url, target_tar);
     }
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    window.open(target_url, target_tar);
-  }
 }
 
 // 외부 링크
-document.querySelectorAll('.kebab-list a').forEach((anchor) => {
-  anchor.addEventListener('click', (event) =>
-    collectAnchorClick(event, `교육서비스:${anchor.innerText}`),
-  );
+document.querySelectorAll("a").forEach((anchor) => {
+    anchor.addEventListener("click", (event) => collectAnchorClick(event, `교육서비스:${anchor.innerText}`));
 });
 
 //------------------------------------------------------------
 // @post /collect/sql
 function collectSQL() {
-  const session_id = localStorage.getItem('session_id');
-  const contents = window.editor.getValue();
-  fetch(`${BASE_URL}/collect/sql`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Session-Id': session_id,
-    },
-    body: JSON.stringify({ contents }),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
+    const session_id = sessionStorage.getItem("session_id");
+    const contents = window.editor.getValue();
+    fetch(`${BASE_URL}/collect/sql`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Session-Id": session_id,
+        },
+        body: JSON.stringify({ contents }),
     })
-    .catch((error) => console.error('Error:', error));
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+        })
+        .catch((error) => console.error("Error:", error));
 }
